36f1b25fce69f8422ac109ea0289d75892369deb
palinkiewicz <pogromca.ap@gmail.com>
2024-06-08 22:33:29 +0200

    Replace many-to-many relation with one-to-many

--- app/src/main/java/pl/dakil/momentum/data/model/Habit.kt
+++ app/src/main/java/pl/dakil/momentum/data/model/Habit.kt
@@ -2,6 +2,7 @@ package pl.dakil.momentum.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.Date
@@ -10,10 +11,20 @@ import java.util.Date
     tableName = "Habits",
     indices = [
         Index("id", unique = true)
+    ],
+    foreignKeys = [
+        ForeignKey(
+            HabitCountUnit::class,
+            parentColumns = ["id"],
+            childColumns = ["habit_count_unit_id"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
     ]
 )
 data class Habit (
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int = 0,
+    @ColumnInfo(name = "habit_count_unit_id") val habitCountUnitId: Int,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "target") val target: Int,
